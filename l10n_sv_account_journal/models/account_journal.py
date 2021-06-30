7571c8236d912eb329316c0515d113c3a12331b9
Alejandro Olano <alejocode@gmail.com>
2021-06-30 13:39:47 -0500

    [FIX] Acount Journal DGII SV

--- l10n_sv_account_journal/models/account_journal.py
+++ l10n_sv_account_journal/models/account_journal.py
@@ -2,7 +2,7 @@
 # Copyright 2021 Alejandro Olano <Github@alejo-code>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class AccountJournal(models.Model):
@@ -12,9 +12,11 @@ class AccountJournal(models.Model):
         ('1', 'PRINTED BY PRINTER OR TICKETS'), ('2', 'SINGLE FORM')
     ],
                                       string='Class Document')
-    type_document = fields.Selection(
-        selection=[('03',
-                    'Tax Credit Voucher'), ('05',
-                                            'Credit Note'), ('06',
-                                                             'Debit Note')])
-    
+    type_document = fields.Selection(selection=[(
+        '01', 'FINAL CONSUMER INVOICE'
+    ), ('02', 'SIMPLIFIED SALES INVOICE'), (
+        '03', 'Tax Credit Voucher'), ('05', 'Credit Note'), (
+            '06',
+            'Debit Note'), ('10',
+                            'REGISTER MACHINE TICKETS'), ('11',
+                                                          'EXPORT BILL')])
